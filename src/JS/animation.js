ed567b2953a258088bcf3b6bdcc5d0da25a6cb46
Carlos Trujillo <augusto21_20@hotmail.com>
2022-03-05 19:26:20 -0500

    Se añade el script para las animaciones al realizar scroll

---
+++ src/JS/animation.js
@@ -0,0 +1,113 @@
+window.sr = ScrollReveal();
+
+// Animaciones para la página principal (index.html)
+
+sr.reveal('.main-hero__info', {
+    duration: 1400,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.main-hero__image', {
+    duration: 1400,
+    origin: 'right',
+    distance: '400px'
+});
+
+sr.reveal('.main__first-part', {
+    duration: 1700,
+    origin: 'bottom',
+    distance: '500px'
+});
+
+sr.reveal('.main__second-part', {
+    duration: 1700,
+    origin: 'right',
+    distance: '500px'
+});
+
+sr.reveal('.main-article__first-part', {
+    duration: 1600,
+    origin: 'left',
+    distance: '500px'
+});
+
+sr.reveal('.main-article__second-part', {
+    duration: 1500,
+    origin: 'right',
+    distance: '500px'
+});
+
+sr.reveal('.main-article__third-part', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '500px'
+});
+
+sr.reveal('.main-section', {
+    duration: 1400,
+    origin: 'left',
+    distance: '500px'
+});
+
+// Animaciones para la página Our Company (about.html)
+
+sr.reveal('.main-hero__image--about', {
+    duration: 1400,
+    origin: 'right',
+    distance: '400px'
+});
+
+sr.reveal('.main-about__container', {
+    duration: 1700,
+    origin: 'bottom',
+    distance: '600px'
+});
+
+// Animaciones para la página Location (location.html)
+sr.reveal('.main-locations__canada', {
+    duration: 1700,
+    origin: 'right',
+    distance: '600px'
+});
+
+sr.reveal('.main-locations__australia', {
+    duration: 1700,
+    origin: 'left',
+    distance: '600px'
+});
+
+sr.reveal('.main-locations__uk', {
+    duration: 1700,
+    origin: 'right',
+    distance: '600px'
+});
+
+// Animaciones para la página Contact (contact.html)
+sr.reveal('.main-form__info', {
+    duration: 1500,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.container-form', {
+    duration: 1500,
+    origin: 'right',
+    distance: '400px'
+});
+
+// Animaciones para las páginas de servicios
+sr.reveal('.main-hero-services', {
+    duration: 1500,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.main-services', {
+    duration: 1800,
+    origin: 'bottom',
+    distance: '800px'
+});
+
+
+
